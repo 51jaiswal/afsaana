5b03c1d86188d90cb80ae429fcde29cf614e6fb4
51jaiswal <shubham51jaiswal@gmail.com>
2019-04-21 02:55:03 +0530

    Added Contactus

--- src/main/python/server.py
+++ src/main/python/server.py
@@ -9,5 +9,10 @@ def comming_soon():
 @app.route('/home')
 def home():
    return render_template("home.html")
+
+@app.route('/contact')
+def contact():
+   return render_template("contact.html")
+
 if __name__ == '__main__':
    app.run(host='0.0.0.0', debug=True, port=5000)
